a0c5253bbc3526170da8dff380eb6680883f4f65
igarasy <igarasshi.lucass@gmail.com>
2022-02-15 22:00:05 -0300

    Criando a página de login

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login/*" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
