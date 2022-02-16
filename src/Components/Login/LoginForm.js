819a12bcf25e0f2c48c7535821904075af0d97db
igarasy <igarasshi.lucass@gmail.com>
2022-02-16 20:58:34 -0300

    Criando Hook de formulário

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = useForm();
+  const password = useForm();
+  console.log(password.value);
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-      });
+    if (username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(),
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+        });
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
---
+++ src/Hooks/useForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const types = {
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Preencha um e-mail válido',
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor.');
+      return false;
+    } else if (types[type] && types[type].regex.test(value)) {
+      setError(types[type].message);
+      /* O types[type] acessará a string que recebermos ao chamarmos o useForm, nesse caso será 'e-mail' quando for username */
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    /* Podemos exportar o validate como método, para ser ativado assim que for chamado */
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
