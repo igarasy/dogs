2846d84cb719ad69c2c082b232179fc9ef3c67bd
igarasy <igarasshi.lucass@gmail.com>
2022-02-22 20:57:43 -0300

    Criando os componentes do feed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className={styles.footer}></div>;
 };
 
 export default Footer;
