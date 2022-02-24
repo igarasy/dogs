226bbe847f09299b6a50fde091d34b6e9135b026
igarasy <igarasshi.lucass@gmail.com>
2022-02-23 21:52:13 -0300

    Criando botão de deletar a foto

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
 import styles from '../Photo/PhotoContent.module.css';
 import PhotoComments from './PhotoComments';
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
   const { photo, comments } = data;
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
@@ -13,7 +17,12 @@ const PhotoContent = ({ data }) => {
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className="title">
