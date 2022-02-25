5e2eb786f717ac4595874a9237b6a618f1d5b4c2
igarasy <igarasshi.lucass@gmail.com>
2022-02-25 19:13:04 -0300

    Corrige redirecionamento para o forumulário de  login ao atualizar a página

--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -4,7 +4,13 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
   const { login } = React.useContext(UserContext);
-  return login ? children : <Navigate to="/login" />;
+  if (login === true) {
+    return children;
+  } else if (login === false) {
+    return <Navigate to="/login" />;
+  } else {
+    return <></>;
+  }
 };
 
 export default ProtectedRoute;
