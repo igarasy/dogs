2698f576e9e702524176e7c71f86e2d3f5dc49b5
igarasy <igarasshi.lucass@gmail.com>
2022-02-28 14:30:45 -0300

    Adiciona página de erro 404

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import LoginForm from './LoginForm';
-import LoginCreate from './LoginCreate';
-import LoginPasswordLost from './LoginPasswordLost';
-import LoginPasswordReset from './LoginPasswordReset';
-import { UserContext } from '../../UserContext';
-import styles from './Login.module.css';
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import LoginCreate from "./LoginCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
+import { UserContext } from "../../UserContext";
+import styles from "./Login.module.css";
+import NotFound from "../NotFound";
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
@@ -19,6 +20,7 @@ const Login = () => {
           <Route path="criar" element={<LoginCreate />} />
           <Route path="perdeu" element={<LoginPasswordLost />} />
           <Route path="resetar" element={<LoginPasswordReset />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </section>
--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { UserContext } from '../../UserContext';
-import Feed from '../Feed/Feed';
-import UserHeader from './UserHeader';
-import UserPhotoPost from './UserPhotoPost';
-import UserStats from './UserStats';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import Feed from "../Feed/Feed";
+import NotFound from "../NotFound";
+import UserHeader from "./UserHeader";
+import UserPhotoPost from "./UserPhotoPost";
+import UserStats from "./UserStats";
 
 const User = () => {
   const { data } = React.useContext(UserContext);
@@ -16,6 +17,7 @@ const User = () => {
         <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
   );
