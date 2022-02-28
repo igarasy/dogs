2015a60823fd169217f05177d77aa28ed4ca85e9
igarasy <igarasshi.lucass@gmail.com>
2022-02-28 14:25:05 -0300

    Cria página individual do usuário e suas postagens

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { UserStorage } from "./UserContext";
 import User from "./Components/User/User";
 import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
